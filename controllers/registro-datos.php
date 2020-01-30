bd4ffc876606a53358938bb2fe294f2eacacd0e1
AldoChagollan <aldochagollan@outlook.com>
2020-01-30 10:31:53 -0600

    vista-registro

--- controllers/registro-datos.php
+++ controllers/registro-datos.php
@@ -1,38 +1,17 @@
 <?php
 include '../Modelos/Archivos.php';
 require_once '../Modelos/Conexion.php';
-include '../Modelos/Email.php';
-
-$emailClass = new Modelos\Email();
-
-$email = $_POST['TEmail'];
-$password = $_POST['TPass'];
-$nombre = $_POST['TNombre'];
-$telefono = $_POST['TTelefono'];
-$vkey = $emailClass->setEmail($email);
-$verificado = 0;
-$encriptado = trim(password_hash($password, PASSWORD_DEFAULT));
-
 $conexion = new Modelos\Conexion();
 
-    $consulta_verificar = "SELECT * FROM usuario WHERE email = ?";
-    $datos_verificar = array($_POST['TEmail']);
-    $resultado = json_encode($conexion->consultaPreparada($datos_verificar,$consulta_verificar,2,'s', false, null));
-    if($resultado != '[]'){
-        echo 'Existe';
-    }else{
-          $consulta_registro = 
-          "INSERT INTO usuario (nombre, edad, escolaridad, imagen, telefono, email, password,
-           vkey, verificado, tipo, estado, municipio, trabajo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-          $datos_registro = array($nombre, $telefono, $email, $encriptado, $vkey, $verificado);
-          $resultado = $conexion->consultaPreparada($datos_registro,$consulta_registro,1,'sissssssissss', false, 3);
-          if($resultado == 1){
-            echo $resultado;
-            $enviar = $emailClass->enviarEmailConfirmacion();
-            
-          }
-     echo 'error';     
-          
-    }
+$consulta = "SELECT nombre FROM curso";
+$resultado = json_encode($conexion->obtenerDatosDeTabla($consulta));
+
+foreach($resultado as $opciones);
+
+
+echo '<option value="'.$valores[id].'">'.$valores[paises].'</option>';
+
+
+     
 
 ?>
--- js/registerpage.js
+++ js/registerpage.js
@@ -1,19 +1,85 @@
-$(document).ready(function() {
-    pintar_Estados_Mexico('registrar-estado2');
-    function pintar_Estados_Mexico(comboBox){
-        var datos_estado_mexico = [];
-        var i = 0;
-      datos_estado_mexico = ["Aguascalientes","Baja California","Baja California Sur","Campeche","Chiapas","Chihuahua",
-      "Ciudad de México","Coahuila de Zaragoza","Colima","Durango","México","Guanajuato","Guerrero","Hidalgo","Jalisco",
-      "Michoacán de Ocampo","Morelos","Nayarit","Nuevo León","Oaxaca","Puebla","Querétaro","Quintana Roo","San Luis Potosí",
-      "Sinaloa","Sonora","Tabasco","Tamaulipas","Tlaxcala","Veracruz de Ignacio de la Llave","Yucatán","Zacatecas"
-      ];
-      let templete = `<option value="">Elejir</option>`;
-      for(i = 0; i < datos_estado_mexico.length; i++){
-        templete +=  `<option value="${datos_estado_mexico[i]}">${datos_estado_mexico[i]}</option>`;
-      }
-      $('#'+comboBox).html(templete);
+$(document).ready(function () {
+  pintar_Estados_Mexico('registrar-estado2');
+  datosUsuario();
 
+  $.ajax({
+    url: "../controllers/registro-datos.php",
+    type: "POST",
+    data: {'peticion':'cargar_listas'},
+
+    success: function (response) {
+      datos=JSON.parse(response);
+      $('#select_curso_tema').html(datos);
+      
+        console.log(datos);
+     
+      
     }
+});
+
+
+  function datosUsuario() {
+
+    $(document).on('click','#btn-bloque-añadir', function(){
+      var cantidad = $('#nombre-bloque').val();
+      template = '';
+      template +=
+        `
+        <tr class="del">
+        <th scope="row">${cantidad}</th>
+        <td>
+        <button id="borrar-bloque">borrar</button>
+        </td>
+        </tr>
+        
+        `;
+      $('#datos-bloque').append(template);
+      $('#nombre-bloque').val("");
+
+    });
+
+    $(document).on('click','#borrar-bloque', function(){
+      $(this).parent().parent().remove();
+    });
+
+    $(document).on('click','#btn-bloque', function(){
+      var valores="";
+
+        $('#datos-bloque').find("th").each(function(){
+            valores+=$(this).html()+"\n";
+        });
+
+      $.ajax({
+            url: "../controllers/registro-datos.php",
+            type: "POST",
+            data: 'valores='+valores,
+
+            success: function (response) {
+              if(response){
+                console.log("entra: "+response);
+              }
+              
+            }
+        });
+
+    });
+    
+}
+
+  function pintar_Estados_Mexico(comboBox) {
+    var datos_estado_mexico = [];
+    var i = 0;
+    datos_estado_mexico = ["Aguascalientes", "Baja California", "Baja California Sur", "Campeche", "Chiapas", "Chihuahua",
+      "Ciudad de México", "Coahuila de Zaragoza", "Colima", "Durango", "México", "Guanajuato", "Guerrero", "Hidalgo", "Jalisco",
+      "Michoacán de Ocampo", "Morelos", "Nayarit", "Nuevo León", "Oaxaca", "Puebla", "Querétaro", "Quintana Roo", "San Luis Potosí",
+      "Sinaloa", "Sonora", "Tabasco", "Tamaulipas", "Tlaxcala", "Veracruz de Ignacio de la Llave", "Yucatán", "Zacatecas"
+    ];
+    let templete = `<option value="">Elejir</option>`;
+    for (i = 0; i < datos_estado_mexico.length; i++) {
+      templete += `<option value="${datos_estado_mexico[i]}">${datos_estado_mexico[i]}</option>`;
+    }
+    $('#' + comboBox).html(templete);
+
+  }
 
 });
