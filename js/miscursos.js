79019ffe02aba0a4b55bb747a50f49579eed0a2c
Uriel Cisneros <urielcisne96@gmail.com>
2020-02-07 13:38:32 -0600

    fusion

--- js/miscursos.js
+++ js/miscursos.js
@@ -19,11 +19,12 @@ $(document).ready(function () {
                     for (i = 0; i < datos.length; i++) {
                         template +=
                             `
-                <tr>
-                    <th id="imagen-curso" class="align-middle" style="width: 12rem;" scope="row"><img src=${datos[i][5]} alt="logo-html5" width="100%"></th>
-                    <td id="nombre-curso" class="align-middle"><h2 class="h3">${datos[i][3]}</h2></td>
-                    <td id="descripcion-curso" class="align-middle"><h4 class="h4">${datos[i][4]}</h4></td>
-                    <td id="progreso-curso" class="align-middle"><h3 class="h1"></h3></td>
+                 <tr>
+                    <a data-idcurso="${datos[i][2]}">
+                        <th id="imagen-curso" class="align-middle" style="width: 12rem;" scope="row"><img src=${datos[i][5]} alt="logo-html5" width="100%"></th>
+                        <td id="nombre-curso" class="align-middle"><h2 class="h3">${datos[i][3]}</h2></td>
+                        <td id="descripcion-curso" class="align-middle"><h4 class="h4">${datos[i][4]}</h4></td>
+                    </a>
                  </tr>
 
                     `;
