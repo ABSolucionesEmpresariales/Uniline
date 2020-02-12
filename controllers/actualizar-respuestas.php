44869e8cb76a18bbea768178e2c6ccbe1f45cc01
Uriel Cisneros <urielcisne96@gmail.com>
2020-02-12 12:33:25 -0600

    Removefiles

--- controllers/actualizar-respuestas.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../Modelos/Conexion.php';
- $conexion = new Modelos\Conexion();
- $conulta = "UPDATE pregunta SET respuestas = 'local mente,#De internet,De steam,Solo de descarga' WHERE idpregunta = ?";
-
- for($i = 1; $i <= 7; $i++){
-    $datos = array($i);
-    echo $conexion->consultaPreparada($datos,$conulta,1,"i",false,null);
- }
