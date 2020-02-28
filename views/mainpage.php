36a0a98b20758e146e55e21ca2d34a4e37aec041
Unknown <unknown@example.com>
2020-02-28 14:15:27 -0600

    Politicas de privacidad
    se agrego la pagina de politicas de privacidad

--- views/mainpage.php
+++ views/mainpage.php
@@ -352,7 +352,7 @@ session_start();
           </p>
           <ul class="list-inline  mt-0 clock text-center">
             <li class="list-inline-item">
-              <a href="#">Politicas de Privacidad</a>
+              <a href="privacyPolicy.php">Politicas de Privacidad</a>
             </li>
             <li class="list-inline-item">
               <a href="#">Imagen Corporativa</a>
