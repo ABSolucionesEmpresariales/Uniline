4e7518b9f256e854c45656729bee0aaed3ff3e50
Uriel Cisneros <urielcisne96@gmail.com>
2020-02-29 12:24:19 -0600

    FullAvicoPrivacidad

--- views/mainpage.php
+++ views/mainpage.php
@@ -349,7 +349,7 @@ session_start();
           </p>
           <ul class="list-inline  mt-0 clock text-center">
             <li class="list-inline-item">
-              <a href="#">Politicas de Privacidad</a>
+              <a href="avisodeprivacidad.php">Politicas de Privacidad</a>
             </li>
             <li class="list-inline-item">
               <a href="#">Imagen Corporativa</a>
